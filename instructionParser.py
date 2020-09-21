cfff72405460c48035ea0266fae87329e8c2dc71
Michael Russell <37958894+michaelrussell4@users.noreply.github.com>
2020-09-21 13:54:54 -0600

    Updated with test folder

--- instructionParser.py
+++ instructionParser.py
@@ -6,24 +6,60 @@ def dec_to_bin(num_dec, bit_len):
 
 class instructionParser:
     def __init__(self, file_lines):
+        self.__data_start, self.__text_start = '0x10000000', '0x400000'
         self.__file_lines = file_lines
         self.__dataSection, self.__dataSectionSize = self.__getDataSection()
         self.__textSection, self.__textSectionSize = self.__getTextSection()
         self.__binaryRepresentation = self.__generateBinary()
-        # self.__binaryRepresentation_lst = [self.__binaryRepresentation[i:i+32] for i in range(0,len(self.__binaryRepresentation), 32)]
+        self.__binaryRepresentation_lst = [self.__binaryRepresentation[i:i+32] for i in range(0,len(self.__binaryRepresentation), 32)]
 
-    # def __instr_to_bin_funct_lookup(self, funct):
-    #     funct_lkup = {
-    #         'add' : lambda rd, rs, rt : f'000000{dec_to_bin(int(rs), 5)}{dec_to_bin(int(rt), 5)}{dec_to_bin(int(rd), 5)}00000100000',
-    #         'addi' : lambda rt, rs, imm : f'001000{dec_to_bin(int(rs), 5)}{dec_to_bin(int(rt), 5)}{self.__dataSection[imm]}',
-    #         'addiu' : lambda rt, rs, imm : f'001001{dec_to_bin(int(rs), 5)}{dec_to_bin(int(rt), 5)}{self.__dataSection[imm]}',
-    #         'addu' : lambda rd, rs, rt : f'000000{dec_to_bin(int(rs), 5)}{dec_to_bin(int(rt), 5)}{dec_to_bin(int(rd), 5)}00000100001',
-    #         'and' : lambda rd, rs, rt : f'000000{dec_to_bin(int(rs), 5)}{dec_to_bin(int(rt), 5)}{dec_to_bin(int(rd), 5)}00000100100',
-    #         'andi' : lambda rt, rs, imm : f'001100{dec_to_bin(int(rs), 5)}{dec_to_bin(int(rt), 5)}{self.__dataSection[imm]}',
-    #         'beq' : lambda rs, rt, ofst : f'000100{dec_to_bin(int(rs), 5)}{dec_to_bin(int(rt), 5)}{self.__dataSection[imm]}'
+    def __instr_to_bin_funct_lookup(self, funct):
+        def beq(rs, rt, tgt_lbl, pc):
+            # conversion calculation part 1. (pc_target - pc_current) / 4
+            # I subtracted -4 to get a positive instead of negative result
+            target_minus_pc = int(self.__dataSection[tgt_lbl], 16) - int(self.__text_start, 16) // -4
+            # subtract one and flip bits to get two's complement representing the actual (negative) answer
+            bin_t_m_p = dec_to_bin(target_minus_pc - 1, 8)
+            twos_complement = ''.join([{'0':'1','1':'0'}[b] for b in bin_t_m_p])
+            offset = dec_to_bin(int(f'0xFF', 16) + twos_complement, 16)
+            return f'000100{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{offset}'
+            
+        def bne(rs, rt, tgt_lbl, pc):
+            # conversion calculation part 1. (pc_target - pc_current) / 4
+            # I subtracted -4 to get a positive instead of negative result
+            target_minus_pc = int(self.__textSection[tgt_lbl], 16) - int(self.__text_start, 16) // -4
+            # subtract one and flip bits to get two's complement representing the actual (negative) answer
+            bin_t_m_p = dec_to_bin(target_minus_pc - 1, 8)
+            twos_complement = ''.join([{'0':'1','1':'0'}[b] for b in bin_t_m_p])
+            offset = dec_to_bin(int(f'0xFF', 16) + twos_complement, 16)
+            return f'000100{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{offset}'
 
-    #     }
-    #     pass
+
+        funct_lkup = {
+            'add' : lambda rd, rs, rt : f'000000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}00000100000',
+            'addi' : lambda rt, rs, imm : f'001000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(int(self.__dataSection[imm], 16), 16)}',
+            'addiu' : lambda rt, rs, imm : f'001001{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(int(self.__dataSection[imm], 16), 16)}',
+            'addu' : lambda rd, rs, rt : f'000000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}00000100001',
+            'and' : lambda rd, rs, rt : f'000000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}00000100100',
+            'andi' : lambda rt, rs, imm : f'001100{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(int(self.__dataSection[imm], 16), 16)}',
+            'beq' : beq, 
+            'bne' : bne,
+            'j' : lambda target_lbl : f'00001000{dec_to_bin(int(self.__textSection[target_lbl], 16), 24)[:-2]}',
+            'jal' : lambda target_lbl : f'00001000{dec_to_bin(int(self.__textSection[target_lbl], 16), 24)[:-2]}',
+            'jr' : lambda rs : f'000000{dec_to_bin(rs, 5)}000000000000000001000',
+            'lui' : lambda rt, imm : f'00111100000{dec_to_bin(rt, 5)}{dec_to_bin(int(self.__dataSection[imm], 16), 16)}',
+            'lw' : lambda rt, offset, base : f'100011{dec_to_bin(base, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(offset, 16)}',
+            'nor' : lambda rd, rs, rt : f'000000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}00000100111',
+            'or' : lambda rd, rs, rt : f'000000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}00000100101',
+            'ori' : lambda rt, rs, imm : f'001101{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(int(self.__dataSection[imm], 16), 16)}',
+            'sll' : lambda rd, rt, sa : f'00000000000{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}{dec_to_bin(sa, 5)}000000',
+            'sltu' : lambda rd, rs, rt : f'000000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}00000101011',
+            'srl' : lambda rd, rt, sa : f'00000000000{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}{dec_to_bin(sa, 5)}000010',
+            'subu' : lambda rd, rs, rt : f'000000{dec_to_bin(rs, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(rd, 5)}00000100011',
+            'sw' : lambda rt, offset, base : f'101011{dec_to_bin(base, 5)}{dec_to_bin(rt, 5)}{dec_to_bin(offset, 16)}',
+            }
+
+        pass
 
     def __convertLaInstruction(self, line):
         # lui and possibly ori
@@ -43,7 +79,6 @@ class instructionParser:
 
     
     def __getDataSection(self):
-        data_strt = '0x10000000'
         pc = 0
         ds = {}
         data_s, text_s = 0, 0
@@ -58,13 +93,12 @@ class instructionParser:
         for dataLine in filteredDataLines:
             if dataLine.find(':') != -1:
                 lbl_split = dataLine.index(':')
-                ds[dataLine[:lbl_split]] = hex(pc + int(data_strt, 16))
+                ds[dataLine[:lbl_split]] = hex(pc + int(self.__data_start, 16))
             pc += 4
 
         return ds, pc // 4
 
     def __getTextSection(self):
-        text_strt = '0x400000'
         pc = 0
         ts = {}
         text_s = 0
@@ -84,7 +118,7 @@ class instructionParser:
         for textLine2 in filteredTextLines:
             if ':' in textLine2:
                 lbl_split = textLine2.index(':')
-                ts[textLine2[:lbl_split]] = hex(pc + int(text_strt, 16))
+                ts[textLine2[:lbl_split]] = hex(pc + int(self.__text_start, 16))
             else:
                 pc += 4
         return ts, pc // 4
