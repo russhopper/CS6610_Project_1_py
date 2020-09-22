fe0248619b9065fdb457c6ce05d8f05473b35efe
Wesley Hooker <wesleyhooker@mail.weber.edu>
2020-09-21 18:56:14 -0600

    Test updated

--- test.py
+++ test.py
@@ -12,12 +12,24 @@ def test():
         f = open(_test_folder + test, "r")
         test_string = f.read()
 
+        #split every 32
+        n = 32
+        output_array = [output_string[i:i+n] for i in range(0, len(output_string), n)]
+        test_array = [test_string[i:i+n] for i in range(0, len(test_string), n)]
+
         # check if they are equal or not
         print(f"Testing: {test}")
+
+        i = 0
+        while i < len(test_array):
+            is_equal = output_array[i] == test_array[i]
+            print(f"Test: {test_array[i]} - Output: {output_array[i]} = {is_equal}")
+            i += 1
+        
         if output_string == test_string:
             print("Pass\n")
         else:
-            print(f"{output_string}\n!=\n{test_string}")
+            #print(f"{output_string}\n!=\n{test_string}")
             print("Fail\n")
 
 
